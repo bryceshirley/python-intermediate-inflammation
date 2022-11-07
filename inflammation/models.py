285b98bdc602f09cafd86de12d6dfcd73b2916bc
~bryceshirley <~bryce.shirley@stfc.ac.uk>
2022-11-07 10:19:29 +0000

    New stdandard Deiviation function

--- inflammation/models.py
+++ inflammation/models.py
@@ -32,3 +32,6 @@ def daily_min(data):
     """Calculate the daily min of a 2d inflammation data array."""
     return np.min(data, axis=0)
 
+def daily_std(data):
+    """Calculate the daily SD of a 2d inflammation data array."""
+    return np.std(data, axis=0)
